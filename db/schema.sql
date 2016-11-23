2b7ef02690f158b7b3b4f6f8b8257a15bfea36f0
Ben Cromwell <bencromwell@wems.co.uk>
2016-11-23 12:15:40 +0000

    add a note to the schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,6 @@
+-- Database schema
+-- This should be written in such a way that it can be run repeatedly - i.e. it is idempotent
+
 CREATE TABLE IF NOT EXISTS `certs` (
   `serial_number` int,
   `public_key` text,
